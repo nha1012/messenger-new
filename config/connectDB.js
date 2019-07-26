788fc5d3602672f8b1a7edd82cdb6e40e097de57
nhagoober <nguyenphucnha0111@gmail.com>
2019-07-26 23:03:57 +0700

    me

---
+++ config/connectDB.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+import bluebird from 'bluebird'
+let connectDb = ()=>{
+  mongoose.Promise= bluebird;
+  let DB_CONNECTION = "mongodb";
+  let DB_HOST = "localhost";
+  let DB_PORT = "27017";
+  let DB_NAME = "messenger";
+  let DB_USER = "";
+  let DB_PASSWORD = "";
+
+  let URI = `${DB_CONNECTION}://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+  console.log('connected!');
+  
+  return mongoose.connect(URI,{useNewUrlParser: true});
+}
+module.exports = connectDb;
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-// const express = require('express') 
 import express from 'express'
+import connectDb from './config/connectDB'
+import Contact from './model/Contact'
+//ket noi voi csdl mongodb voi host 27017
+connectDb();
 let app = express();
 app.listen(3000,'localhost',()=>(
   console.log("Server is running on host 3000")
 ))
-app.get('/',(req,res)=>{    
-  res.send('Node is working')
+app.get('/', async (req,res)=>{   
+  try {
+    let item={
+      userId:"0000000000000000000000000000",
+      contactId:"00000000000000000000000000"
+    }
+    let contact = await Contact.create(item)
+    await res.send(contact)
+  } catch (error) {
+    
+  } 
 })
 app.get('/nha',(req,res)=>{
   res.send('router nha')

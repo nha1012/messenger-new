ce1ac8bdb81e95a39ad10093a0451463e4b1a3f3
nha1012 <46342451+nha1012@users.noreply.github.com>
2019-09-22 17:34:57 +0700

    Update viewEngine.js
    a

--- config/viewEngine.js
+++ config/viewEngine.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import expressEjsExtend from 'express-ejs-extend';
 let configEjs = (app)=>{
-  app.use(express.static("./public"))
+  app.use(express.static(__dirname +"./public"))
   app.engine("ejs",expressEjsExtend)
   app.set("view engine","ejs")
   app.set('views','./views')

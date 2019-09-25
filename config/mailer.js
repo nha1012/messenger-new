3eba346fbab0554e8e9e87eb8c8d65bff050b33d
nha1012 <46342451+nha1012@users.noreply.github.com>
2019-09-25 20:43:07 +0700

    Update mailer.js

--- config/mailer.js
+++ config/mailer.js
@@ -5,13 +5,13 @@ let tranSporter = nodeMailer.createTransport({
   secure: false,
   requireTLS: true,
   auth: {
-      user: 'nguyenphucnha0111@gmail.com',
-      pass: '01689753582'
+      user: 'nguyenphucnha111@gmail.com',
+      pass: 'nhajavascript'
   }
 })
 const mailOptions = function (to,subject,text) {
   return {
-    from:'nguyenphucnha0111@gmail.com',
+    from:'nguyenphucnha111@gmail.com',
     to:to,
     subject: subject,
     text:text

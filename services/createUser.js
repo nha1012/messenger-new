25e5c02c00b9883ca7451c911809c042cb94e922
nha1012 <46342451+nha1012@users.noreply.github.com>
2019-09-26 21:41:48 +0700

    Update createUser.js

--- services/createUser.js
+++ services/createUser.js
@@ -1,7 +1,7 @@
 import userSchema from '../model/User'
 import uuidv4 from'uuid/v4'
 import mailerConfig from './../config/mailer'
-let createUser =  (email,gender,password,req)=>{
+let createUser =  (email,gender,password,req,res)=>{
     let userItem={
     userName: email.split('@')[0],
     gender:gender,
@@ -16,6 +16,7 @@ let createUser =  (email,gender,password,req)=>{
     //let arraySuccess = ['Chúng tôi đã gửi email xắc thực tài khoản cho bạn, hãy kiểm tra lại nhé!!']
     //req.flash('success', arraySuccess)
     mailerConfig.tranSporter.sendMail(mailerConfig.mailOptions(email,'Đây là email xác thực tài khoản.','https://messenger-nhadev.herokuapp.com/verify/'+ userItem.local.verifytoken))
+    return res.redirect('/')
   } catch (error) {
     let arrayErrors=['Đã có lỗi xảy ra, vui lòng tạo lại tài khoản, hoặc liên hệ với tôi (Nhã nguyễn) ']
     req.flash('errors', arrayErrors)
